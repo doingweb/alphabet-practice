17e167357c3503e0e28c07e0442b140f2d0acfb1
Chris Antes <chris@chrisdoingweb.com>
2020-06-30 23:12:19 -0700

    Add lowercase letters

--- main.py
+++ main.py
@@ -14,17 +14,20 @@ print("LOVE YOU FINN! 🐴")
 DISPLAY = board.DISPLAY
 FONT = bitmap_font.load_font('/fonts/OldStandardTT-Bold-100.bdf')
 PALETTE_SIZE = 2
-ALPHABET = list(map(chr, range(ord('A'), ord('Z')+1)))
+ALPHABET_UPPERCASE = list(map(chr, range(ord('A'), ord('Z')+1)))
+ALPHABET_LOWERCASE = list(map(chr, range(ord('a'), ord('z')+1)))
 
 fgColor = 0x0000FF
 bgColor = 0xFFFFFF
 letterIndex = 0
+alphabet = ALPHABET_UPPERCASE
 
 leftButton = Debouncer(lambda: pybadger.button.left == 0)
 rightButton = Debouncer(lambda: pybadger.button.right == 0)
+selectButton = Debouncer(lambda: pybadger.button.select == 0)
 
 def displayLetter():
-  letter = ALPHABET[letterIndex]
+  letter = alphabet[letterIndex]
   palette = displayio.Palette(PALETTE_SIZE)
   palette[0] = bgColor # Background
   palette[1] = fgColor # Foreground
@@ -50,25 +53,33 @@ def goToLetter(newIndex):
   displayLetter()
 
 def forwardLetter():
-  if letterIndex == len(ALPHABET) - 1:
+  if letterIndex == len(alphabet) - 1:
     goToLetter(0)
   else:
     goToLetter(letterIndex + 1)
 
 def backLetter():
   if letterIndex == 0:
-    goToLetter(len(ALPHABET) - 1)
+    goToLetter(len(alphabet) - 1)
   else:
     goToLetter(letterIndex - 1)
+
+def switchLetterCase():
+  global alphabet
+  alphabet = ALPHABET_LOWERCASE if alphabet == ALPHABET_UPPERCASE else ALPHABET_UPPERCASE
+  displayLetter()
 
 displayLetter()
 
 while True:
   leftButton.update()
   rightButton.update()
+  selectButton.update()
 
   if leftButton.fell:
     backLetter()
   elif rightButton.fell:
     forwardLetter()
 
+  if selectButton.fell:
+    switchLetterCase()
